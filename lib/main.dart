4f1edf9d842ef14e8b52743810c2b5ff2a864e84
AhmadAkram52 <ahmadg03025249091@gmail.com>
2024-01-22 01:23:15 +0500

    intial

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AChatApp());
+}
+
+class AChatApp extends StatelessWidget {
+  const AChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
