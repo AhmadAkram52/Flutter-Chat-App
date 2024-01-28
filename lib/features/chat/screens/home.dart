823d90118fe37f805a597ebe06d4ddcedc5f7bb5
AhmadAkram52 <ahmadg03025249091@gmail.com>
2024-01-28 23:10:29 +0500

    some Update

--- lib/features/chat/screens/home.dart
+++ lib/features/chat/screens/home.dart
@@ -4,7 +4,6 @@ import 'package:a_chat/features/chat/screens/widgets/home_app_bar.dart';
 import 'package:a_chat/features/chat/screens/widgets/home_floating_button.dart';
 import 'package:a_chat/util/apis/firebase_instances.dart';
 import 'package:a_chat/util/constants/sizes.dart';
-import 'package:a_chat/util/helpers/auth_helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -22,56 +21,37 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: const AHomeAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(ASizes.sm),
-        child: Center(
-          child: FutureBuilder(
-            future: AuthHelper().getAdditionalUserDataFromLocalStorage(),
-            builder: (context, sn) {
-              if (sn.hasData) {
-                return Column(
-                  children: [
-                    Expanded(
-                      child: StreamBuilder(
-                        stream: Apis.fireStore.collection('users').snapshots(),
-                        builder: (context, sn) {
-                          switch (sn.connectionState) {
-                            case ConnectionState.waiting:
-                            case ConnectionState.none:
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            case ConnectionState.active:
-                            case ConnectionState.done:
-                              final data = sn.data!.docs;
-                              userList = data
-                                      .map((e) =>
-                                          ChatUserModel.fromJson(e.data()))
-                                      .toList() ??
-                                  [];
-                              if (userList.isNotEmpty) {
-                                return ListView.builder(
-                                  physics: const BouncingScrollPhysics(),
-                                  itemCount: userList.length,
-                                  itemBuilder: (context, i) {
-                                    return AChatUserCard(
-                                      user: userList[i],
-                                    );
-                                  },
-                                );
-                              } else {
-                                return const Center(
-                                    child: Text("No Connection Found"));
-                              }
-                          }
-                        },
-                      ),
-                    ),
-                  ],
+        child: StreamBuilder(
+          stream: Apis.fireStore.collection('users').snapshots(),
+          builder: (context, sn) {
+            switch (sn.connectionState) {
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
-              } else {
-                return const Text("No Content");
-              }
-            },
-          ),
+              case ConnectionState.active:
+              case ConnectionState.done:
+                final data = sn.data!.docs;
+                userList = data
+                        .map((e) => ChatUserModel.fromJson(e.data()))
+                        .toList() ??
+                    [];
+                if (userList.isNotEmpty) {
+                  return ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: userList.length,
+                    itemBuilder: (context, i) {
+                      return AChatUserCard(
+                        user: userList[i],
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(child: Text("No Connection Found"));
+                }
+            }
+          },
         ),
       ),
       floatingActionButton: const HomeFloatingButton(),
