7519b7338f67893c12117a056564b25bb9d76703
AhmadAkram52 <ahmadg03025249091@gmail.com>
2024-01-30 03:11:10 +0500

    some updates

--- lib/features/chat/screens/home/home.dart
+++ lib/features/chat/screens/home/home.dart
@@ -1,7 +1,7 @@
 import 'package:a_chat/common/widgets/chat_user_card.dart';
 import 'package:a_chat/common/widgets/floating_action_button.dart';
 import 'package:a_chat/features/chat/models/chat_user_model.dart';
-import 'package:a_chat/features/chat/screens/widgets/home_widgets/home_app_bar.dart';
+import 'package:a_chat/features/chat/screens/home/widgets/home_app_bar.dart';
 import 'package:a_chat/util/apis/firebase_instances.dart';
 import 'package:a_chat/util/constants/sizes.dart';
 import 'package:flutter/material.dart';
--- lib/features/chat/screens/profile/profile.dart
+++ lib/features/chat/screens/profile/profile.dart
@@ -1,15 +1,38 @@
 import 'package:a_chat/common/widgets/bar/app_bar.dart';
 import 'package:a_chat/common/widgets/elevated_button_with_Icon.dart';
+import 'package:a_chat/features/chat/controllers/profile/profile_controller.dart';
+import 'package:a_chat/features/chat/screens/profile/widgets/profile_image.dart';
 import 'package:a_chat/util/apis/firebase_instances.dart';
 import 'package:a_chat/util/constants/sizes.dart';
 import 'package:a_chat/util/helpers/auth_helper_functions.dart';
+import 'package:a_chat/util/helpers/user_helper_functions.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'widgets/profile_widgets/profile_image.dart';
-
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final ProfileController ctrl = Get.put(ProfileController());
+
+  @override
+  initState() {
+    ctrl.nameController.text = AUserHelperFunctions.user.displayName!;
+    final dataRef = Apis.fireStore
+        .collection('users')
+        .doc(AUserHelperFunctions.user.uid)
+        .get()
+        .then((DocumentSnapshot value) {
+      final data = value.data() as Map<String, dynamic>;
+      ctrl.aboutController.text = data['about'].toString();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,22 +46,19 @@ class ProfileScreen extends StatelessWidget {
           padding: const EdgeInsets.all(ASizes.defaultSpace),
           child: Center(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ///Profile Image
                 const ProfileImage(),
                 const SizedBox(height: ASizes.spaceBtwItems),
 
                 /// Email
-                Text(
-                  Apis.auth.currentUser!.email.toString(),
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
+                Text(Apis.auth.currentUser!.email.toString(),
+                    style: Theme.of(context).textTheme.headlineSmall),
                 const SizedBox(height: ASizes.spaceBtwSections),
 
                 /// Name Field
                 TextFormField(
+                  controller: ctrl.nameController,
                   decoration: const InputDecoration(
                     labelText: 'Name',
                     prefixIcon: Icon(Icons.person),
@@ -48,6 +68,7 @@ class ProfileScreen extends StatelessWidget {
 
                 /// About Field
                 TextFormField(
+                  controller: ctrl.aboutController,
                   decoration: const InputDecoration(
                     labelText: 'About',
                     prefixIcon: Icon(Icons.error),
